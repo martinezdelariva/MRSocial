d6043343fc430d22a56dd9f1fc0966330bb9d1cc
martinezdelariva <martinezdelariva@gmail.com>
2014-02-18 09:28:10 +0100

    Initial Commit

---
+++ MRSocialAuth/MRViewController.h
@@ -0,0 +1,13 @@
+//
+//  MRViewController.h
+//  MRSocialAuth
+//
+//  Created by Jose Luis Martinez de la Riva on 18/02/14.
+//  Copyright (c) 2014 Jose Luis Martinez de la Riva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRViewController : UIViewController
+
+@end
